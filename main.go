03d514ca7219a1f66bcae2cf824017b3d3f08664
Smit Kumbhani <41439439+smit8800@users.noreply.github.com>
2023-11-18 13:57:43 -0500

    Initiating main file
    build the simplest service you can using the standard packages in the Go programming language.

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello Wold!")
+		d, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		log.Println("Data: ", d)
+		fmt.Fprintf(w, "Hello  %s ", d)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("GoodBye Wold!")
+	})
+
+	http.ListenAndServe("127.0.0.1:8000", nil)
+}
+
+
