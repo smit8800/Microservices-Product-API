41c3318854f20da11fd1213cfe4abd4846692413
Smit Kumbhani <smitkumbhani080@gmail.com>
2024-01-02 14:38:01 -0500

    implemented Byte to String

--- main.go
+++ main.go
@@ -9,18 +9,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello Wold!")
+		log.Println("Hello World!")
 		d, err := io.ReadAll(r.Body)
 		if err != nil {
 			http.Error(w, "Oops", http.StatusBadRequest)
 			return
 		}
-		log.Println("Data: ", d)
-		fmt.Fprintf(w, "Hello  %s ", d)
+		log.Println("Data: ", string(d))
+		fmt.Fprintf(w, "Hello  %s ", string(d))
 	})
 
 	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("GoodBye Wold!")
+		log.Println("GoodBye World!")
 	})
 	http.ListenAndServe("127.0.0.1:8000", nil)
 }
