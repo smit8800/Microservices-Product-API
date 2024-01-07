ad53516513144fef6879f3f36cdb89eebc4d8223
Smit Kumbhani <smitkumbhani080@gmail.com>
2024-01-07 00:52:15 -0500

    add add product functionality and added support for handling it's POST request

--- handlers/products.go
+++ handlers/products.go
@@ -16,16 +16,32 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		p.GetProducts(rw, r)
+		p.getProducts(rw, r)
+		return
+	} else if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
 		return
 	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
+func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to Marshal JSON", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Post Called ")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to Marshal JSON", http.StatusInternalServerError)
+	}
+	http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+
+	data.AddProduct(prod)
 }
