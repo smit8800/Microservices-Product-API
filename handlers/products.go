2e1d3b7137ac5c3e31d3803e9435ac066d52c5d8
Smit Kumbhani <smitkumbhani080@gmail.com>
2024-01-08 02:47:38 -0500

    Added product validation at MiddleWareHandler

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"project/data"
@@ -69,6 +70,18 @@ func (p *Products) MiddlewareValidateProduct(next http.Handler) http.Handler {
 		if err != nil {
 			p.l.Println("[ERROR] deserializing product", err)
 			http.Error(rw, "Error reading product", http.StatusBadRequest)
+			return
+		}
+
+		// validate product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(
+				rw,
+				fmt.Sprintf("Error valdiating product: %s", err),
+				http.StatusBadRequest,
+			)
 			return
 		}
 
