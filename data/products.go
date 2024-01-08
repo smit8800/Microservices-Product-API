2724dab4436e43632abfb3dca27f610aab58e758
Smit Kumbhani <smitkumbhani080@gmail.com>
2024-01-07 19:40:35 -0500

    added updateProduct func based on id

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -59,6 +60,27 @@ func GetProducts() Products {
 func AddProduct(p *Product) {
 	p.ID = getNextID()
 	productList = append(productList, p)
+}
+
+var ErrProductNotFound = fmt.Errorf("Product not found")
+
+func UpdateProduct(id int, p *Product) error {
+	_, pos, err := findProdcut(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id
+	productList[pos] = p
+	return nil
+}
+
+func findProdcut(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+	return nil, -1, ErrProductNotFound
 }
 
 func getNextID() int {
